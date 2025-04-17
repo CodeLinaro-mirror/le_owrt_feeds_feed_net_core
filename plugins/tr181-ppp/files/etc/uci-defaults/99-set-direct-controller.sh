4bc3d1b212e1337348e3ace7083bc9db146137d4
sahbot <support.opensource@softathome.com>
2025-04-17 16:10:15 +0200

    Use $IPKG_INSTROOT path prefix on /etc/uci-defaults scripts that could be executed on host (HOP-9501)
    Problem: When overlayfs is not mounted /etc/uci-defaults scripts
    cannot modify other scripts.
    
    Cause: Writable partitions must not be allowed to contain executable code
    
    Solution: openwrt services can be modified at build time if paths
    are prefixed with $IPKG_INSTROOT. When building the rootfs image, makefile
    will grep for this variable name in the /etc/uci-defaults scripts and when
    is found it will:
     - run the script with $IPKG_INSTROOT set to the rootfs source dir; script
       must return success code, otherwise rootfs image creation will fail
     - script gets removed from rootfs
    
    Issue: HOP-9501 - Disable overlayfs mounting on top of rootfs
    
    Signed-off-by: Alin Nastac <alin.nastac@softathome.com>
    GitOrigin-RevId: 730f5f44d146b595014a38328fc21d59fb3ccf04

--- plugins/tr181-ppp/files/etc/uci-defaults/99-set-direct-controller.sh
+++ plugins/tr181-ppp/files/etc/uci-defaults/99-set-direct-controller.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's|"mod_name": "mod-ppp-uci"|"mod_name": "mod-ppp-direct"|' /etc/ppp/ip-down.d/ppp-down.sh
-sed -i 's|"mod_name": "mod-ppp-uci"|"mod_name": "mod-ppp-direct"|' /etc/ppp/ip-up.d/ppp-up.sh
+sed -i 's|"mod_name": "mod-ppp-uci"|"mod_name": "mod-ppp-direct"|' "$IPKG_INSTROOT/etc/ppp/ip-down.d/ppp-down.sh"
+sed -i 's|"mod_name": "mod-ppp-uci"|"mod_name": "mod-ppp-direct"|' "$IPKG_INSTROOT/etc/ppp/ip-up.d/ppp-up.sh"
 
 exit 0
